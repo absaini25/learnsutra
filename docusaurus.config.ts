d41f9c7feb851182babb09c5fe7ffbddc79db50a
Abhisar Saini <abhisarsaini1@gmail.com>
2024-11-30 14:49:16 +0530

    minor changes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/absaini25/learnsutra',
         },
         blog: {
           showReadingTime: true,
@@ -57,7 +57,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/absaini25/learnsutra',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -135,12 +135,12 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/absaini25/learnsutra',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} LearnSutra, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} LearnSutra.org, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
